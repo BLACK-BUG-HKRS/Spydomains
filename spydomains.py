4f846a74de94ffe758990b7fe18c3251cc0eff36
Major2000 <majormadobe@gmail.com>
2022-10-27 11:34:11 +0300

    func: update spydomains.py scan_subdomains

--- spydomains.py
+++ spydomains.py
@@ -5,3 +5,24 @@ from queue import Queue
 q = Queue()
 list_lock = Lock()
 discovered_domains = []
+
+
+def scan_subdomains(domain):
+    global q
+    while True:
+        
+        subdomain = q.get()
+        
+        url = f"https://{subdomain}.{domain}"
+        try:
+            requests.get(url)
+        except requests.ConnectionError:
+            pass
+        else:
+            print("[+] Discovered subdomain:", url)
+            
+            with list_lock:
+                discovered_domains.append(url)
+
+        
+        q.task_done()
